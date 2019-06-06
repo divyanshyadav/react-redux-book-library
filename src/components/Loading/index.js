f8795028779ec7cbf516d963a22c943183a3d3d6
DY <dy@Hmac.local>
2019-06-06 23:25:34 +0530

    update loading indicator

--- src/components/Loading/index.js
+++ src/components/Loading/index.js
@@ -1,46 +1,20 @@
 import './loading.css';
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 
-const LOADING_MESSAGE = 'Loading';
+const spinner = require('./spinner.gif');
 
-class Loading extends Component {
-    constructor(props) {
-        super(props);
-        this.dots = 0;
+const Loading = ({ show }) => {
+    if (!show) {
+        return null;
     }
 
-    state = {
-        loadingMessage: LOADING_MESSAGE,
-    };
-
-    componentDidMount() {
-        this.interval = setInterval(() => {
-            this.dots += 1;
-            this.setState({
-                loadingMessage: LOADING_MESSAGE + '.'.repeat(this.dots % 4),
-            });
-        }, 200);
-    }
-
-    componentWillUnmount() {
-        clearInterval(this.interval);
-    }
-
-    render() {
-        const { show } = this.props;
-        const { loadingMessage } = this.state;
-        if (!show) {
-            return null;
-        }
-
-        return (
-            <div className="loading__container">
-                <h1 className="loading__container__message">{loadingMessage}</h1>
-            </div>
-        );
-    }
-}
+    return (
+        <div className="loading__container">
+            <img style={{ width: '100px' }} src={spinner} alt="spinner" />
+        </div>
+    );
+};
 
 Loading.defaultProps = {
     show: false,
